d4f5aee522e62d56173e6330a2b5c5434975c4a1
prazwal bist <prazwalbist69@gmail.com>
2022-06-10 23:04:22 +0545

    js modified

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+var gridSquares;
 var upPressed = false;
 var downPressed = false;
 var leftPressed = false;
@@ -48,7 +49,7 @@ function move() {
 	if (upPressed) {
 		var newTop = positionTop-1;
 
-		var element = document.elementFromPoint(player.offsetLeft, newTop);
+		var element = document.elementFromPoint(player.offsetLeft, newTop -3);
 		if (element.classList.contains('sky') == false) {
 			player.style.top = newTop + 'px';	
 		}
@@ -108,6 +109,7 @@ function myLoadFunction() {
 
 document.addEventListener('DOMContentLoaded', myLoadFunction);
 
+//alienmove
 
 function myclickFunction() {
 	var button = document.getElementsByClassName('start');
@@ -117,27 +119,66 @@ function myclickFunction() {
 	var jet = enter.offsetLeft;
 	
 	setInterval (function(){
-		var random = Math.ceil (Math.random()*900);
+		var random = Math.ceil (Math.random()*1100);
 		enter[0].style.left = random + 'px';
     }, 800);
-	("alien",(a) => {
-		if (Math.random()*900 < 0.0001) {
-			add([
-			  "bomb",
-			   pos(a,pos),
-			   rect(15,15),
-			   area(),
-			   color(0,0,0),
-			   move(DOWN,400)
-			])
-		}
-	})
+
+//createbomb()
+
+    setInterval(function createBomb(){
+	var firstBomb = document.getElementsByTagName('div')[7];
+	var secondBomb = document.createElement('div');
+	firstBomb.appendChild(secondBomb);
+	secondBomb.className="bomb";
+
+//dropbomb
+
+    setInterval(function droppingBomb(){
+	var positionOfBomb = secondBomb.offsetTop;
+	secondBomb.style.top = positionOfBomb + 5 +'px';
+    },8)
+    setInterval(function randomBomb(){
+	var randomZ = Math.floor(Math.random()*1000);
+	secondBomb.style.left = randomZ + 'px';
+    },1400)
+    },1000)
 }
+ //startbutton
 function mycallFunction() {
     var element = document.getElementsByClassName('start');
 	element[0].addEventListener('click', myclickFunction);
 	element[0].style.opacity = '1';
 }
 
-
 document.addEventListener('DOMContentLoaded', mycallFunction); 
+
+
+
+function bombMovement() {
+	let randomIndex = Math.floor(Math.random() * alien.length)
+	let bombStart = alien[randomIndex]
+
+	const bombInterval = setInterval(() => { 
+      if (gridSquares[player].classList.contains('bomb') && lives > 0) {
+		gridSquares[bombStart].classList.remove('bomb')
+		lives -= 1
+		livesCounter.innerHTML = '<h2>${lives}</h2>'
+	  } else if (lives === 0) {
+
+		  gameOver.style.display = 'flex'
+		  clearInterval(interval)
+		  clearInterval(bombInterval)
+
+	  }else if ((bombStart > (width ** 2) - width -1)) {
+
+		gridSquares[bombStart].classList.remove('bomb')
+		clearInterval(bombInterval)
+	  }else {
+		  gridSquares[bombStart].classList.remove('bomb')
+		  bombStart += width
+		  gridSquares[bombStart].classList.add('bomb')
+	  }
+	}, 600)
+};
+
+document.addEventListener('DOMContentLoaded', bombMovement);
